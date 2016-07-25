2e119c5453efe38651d1380f1923264b7667958e
kyrayagami <kingdragon1@live.com.mx>
2016-07-24 22:20:16 -0500

    add files jquery

--- resguardos/resguardos.php
+++ resguardos/resguardos.php
@@ -4,9 +4,9 @@
         <meta charset="UTF-8">
         <title> Resguardos</title>
 		<link href="../js/bootstrap.min.css" rel="stylesheet">
-       <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+       <link rel="stylesheet" href="../js/jquery-ui.css">
+  <script src="../js/jquery-1.10.2.js"></script>
+  <script src="../js/jquery-ui.js"></script>
   <script type="text/javascript">
 $(function() {
             $("#Equipo_Serie").autocomplete({
